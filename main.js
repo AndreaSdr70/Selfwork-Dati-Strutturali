bb223cdd10d655fcc2d8b265b669cc3155d320a0
AndreaSdr70 <andrea@sdringola.it>
2024-07-18 13:42:44 +0200

    Selfwork Dati Strutturali

---
+++ main.js
@@ -0,0 +1,19 @@
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+
+  
+  console.log(array_1[0][0]);
+  console.log(array_1[1][0]);
+  console.log(array_1[0][1]);
